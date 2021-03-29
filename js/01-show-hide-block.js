39f82112798d5107e8460bec8500eafb571ca888
Ann-Gol <0842724@gmail.com>
2021-03-29 17:27:37 +0300

    added section accardeon with function open and hidden content

--- js/01-show-hide-block.js
+++ js/01-show-hide-block.js
@@ -1,19 +1,13 @@
+const button = document.querySelector("#button");
 
-const button = document.querySelector('#button');
+const content = document.querySelector("#content");
 
-const content = document.querySelector('#content');
-
-
-
-button.addEventListener('click', function (){
-
-
-if (content.classList.toggle('content-hidden')){
-    button.textContent = 'Открыть блок';
-} else{
-    button.textContent = 'Закрыть блок';
-}
-
+button.addEventListener("click", function () {
+  if (content.classList.toggle("content-hidden")) {
+    button.textContent = "Открыть блок";
+  } else {
+    button.textContent = "Закрыть блок";
+  }
 });
 
 
